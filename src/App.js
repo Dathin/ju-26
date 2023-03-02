d5ed728461c6934e361261c281da4e75bea8ac90
Pedro Caires <pedro.caires@mheducation.com>
2023-03-02 09:23:21 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './photo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Parabéns Ju!!!
+        </p>
+        <p>
+          🥷 ❤️
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
